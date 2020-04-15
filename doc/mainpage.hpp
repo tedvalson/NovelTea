8e90fa32ad507cc863c237f56667b13a5af63263
Thomas Edvalson <machin3@gmail.com>
2020-04-15 12:26:51 -0400

    Add basic Doxygen files

---
+++ doc/mainpage.hpp
@@ -0,0 +1,21 @@
+////////////////////////////////////////////////////////////
+/// \mainpage
+///
+/// This documentation is not yet complete. Hell, the NovelTea project itself isn't complete.
+///
+/// \section example Simple NovelTea Launcher
+/// Source can be found: src/core/main.cpp
+///
+/// This is the code:
+///
+/// \code
+///
+/// #include <NovelTea.hpp>
+///
+///
+/// int main(int argc, char** argv) {
+/// 	p
+/// 	return 0;
+/// }
+/// \endcode
+////////////////////////////////////////////////////////////
