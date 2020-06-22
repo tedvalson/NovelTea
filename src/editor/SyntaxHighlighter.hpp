cd07a46742df81d3db91a20308242340bbc46912
Thomas Edvalson <machin3@gmail.com>
2020-06-22 13:56:54 -0400

    Script Editor: Add a more complete SyntaxHighlighter for javascript

--- src/editor/SyntaxHighlighter.hpp
+++ src/editor/SyntaxHighlighter.hpp
@@ -8,17 +8,54 @@ QT_BEGIN_NAMESPACE
 class QTextDocument;
 QT_END_NAMESPACE
 
+class BlockData: public QTextBlockUserData
+{
+public:
+	QList<int> bracketPositions;
+};
+
+
 class SyntaxHighlighter : public QSyntaxHighlighter
 {
 	Q_OBJECT
-
 public:
+	enum ColorComponent {
+		Background,
+		Normal,
+		Comment,
+		Number,
+		String,
+		Operator,
+		Identifier,
+		Keyword,
+		BuiltIn,
+		Sidebar,
+		LineNumber,
+		LineNumberActive,
+		Cursor,
+		Marker,
+		BracketMatch,
+		BracketError,
+		FoldIndicator
+	};
+
 	SyntaxHighlighter(QTextDocument *parent = 0);
+
+	void setColor(ColorComponent component, const QColor &color);
+	void mark(const QString &str, Qt::CaseSensitivity caseSensitivity);
+
+	QStringList keywords() const;
+	void setKeywords(const QStringList &keywords);
 
 protected:
 	void highlightBlock(const QString &text) override;
 
 private:
+	QSet<QString> m_keywords;
+	QSet<QString> m_knownIds;
+	QHash<ColorComponent, QColor> m_colors;
+	QString m_markString;
+	Qt::CaseSensitivity m_markCaseSensitivity;
 };
 
 #endif // SYNTAXHIGHLIGHTER_HPP
