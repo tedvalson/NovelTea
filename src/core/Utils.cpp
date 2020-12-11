b71b5e091a8fdddf76616756f1cfdbde9c582967
Thomas Edvalson <machin3@gmail.com>
2020-12-11 18:59:46 -0500

    Core: Fix text wrapping bug

--- src/core/Utils.cpp
+++ src/core/Utils.cpp
@@ -38,6 +38,7 @@ bool wrapText(sf::Text &text, float width)
 	auto processedWidth = 0.f;
 	int pos = 0;
 	std::string out;
+	sf::Vector2f lastWordPos;
 	for (auto &word : words)
 	{
 		auto p = text.findCharacterPos(pos + word.size());
@@ -45,11 +46,12 @@ bool wrapText(sf::Text &text, float width)
 		{
 			out += "\n" + word + " ";
 			pos += word.size() + 1;
-			processedWidth += p.x - processedWidth;
+			processedWidth += lastWordPos.x - processedWidth;
 		} else {
 			out += word + " ";
 			pos += word.size() + 1;
 		}
+		lastWordPos = p;
 	}
 
 	text.setString(out);
