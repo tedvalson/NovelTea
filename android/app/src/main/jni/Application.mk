bab4e76456565c97d385ed0725e4a170fedb36cc
Thomas Edvalson <machin3@gmail.com>
2021-02-22 10:18:42 -0500

    Android: Attempt to fix NDK build for Github CI

--- android/app/src/main/jni/Application.mk
+++ android/app/src/main/jni/Application.mk
@@ -1,8 +1,9 @@
-NDK_TOOLCHAIN_VERSION := clang
+NDK_TOOLCHAIN_VERSION := 4.9
 APP_PLATFORM := android-16
 # APP_STL has to match CMAKE_ANDROID_STL_TYPE
-APP_STL := c++_shared
+APP_STL := c++_static
 APP_ABI := armeabi-v7a
 APP_MODULES := noveltea-launcher
-#APP_OPTIM := release
+APP_OPTIM := release
+APP_CFLAG := -O3
 #APP_CFLAG := -g -ggdb -O0
