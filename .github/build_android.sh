b69ecb705c6c663eb1a40d3261874c833f443acc
Thomas Edvalson <machin3@gmail.com>
2021-12-29 08:53:02 -0500

    Github: Install and run Android app to test it.

---
+++ .github/build_android.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+set -e
+
+ABI_LIST="armeabi-v7a x86"
+
+for ABI in ${ABI_LIST}
+do
+    mkdir -p SFML/build/${ABI}
+    cd SFML/build/${ABI}
+    cmake -DCMAKE_SYSTEM_NAME=Android -DCMAKE_ANDROID_NDK=$1 -DCMAKE_ANDROID_ARCH_ABI=$ABI -DCMAKE_ANDROID_NDK_TOOLCHAIN_VERSION=clang -DCMAKE_ANDROID_STL_TYPE=c++_static -DCMAKE_ANDROID_API=26 ../..
+    cmake --build . --config Release --target install
+    cd -
+
+    mkdir -p build/${ABI}
+    cd build/${ABI}
+    cmake -DBUILD_EDITOR=OFF -DBUILD_TEST=OFF -DBUILD_DOCS=OFF -DCMAKE_SYSTEM_NAME=Android -DANDROID_ABI=$ABI -DCMAKE_ANDROID_NDK=$1 -DCMAKE_ANDROID_ARCH_ABI=$ABI -DCMAKE_ANDROID_NDK_TOOLCHAIN_VERSION=clang -DCMAKE_ANDROID_STL_TYPE=c++_static -DCMAKE_ANDROID_API=26 ../..
+    cmake --build . --config Release --target install
+    cd -
+done
--- android/app/src/main/jni/Application.mk
+++ android/app/src/main/jni/Application.mk
@@ -2,7 +2,7 @@ NDK_TOOLCHAIN_VERSION := 4.9
 APP_PLATFORM := android-16
 # APP_STL has to match CMAKE_ANDROID_STL_TYPE
 APP_STL := c++_static
-APP_ABI := armeabi-v7a
+APP_ABI := all
 APP_MODULES := noveltea-launcher
 APP_OPTIM := release
 APP_CFLAG := -O3
