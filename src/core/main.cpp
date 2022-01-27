645502175c97bc02a1f5c6e5d5ce637047f64db5
Thomas Edvalson <machin3@gmail.com>
2022-01-27 09:01:05 -0500

    Launcher: Add basic argument parsing for loading entites
    The editor already passes these args when launching

--- src/core/main.cpp
+++ src/core/main.cpp
@@ -11,15 +11,43 @@ int main(int argc, char **argv)
 	GSettings.setDirectory(dir);
 	GSettings.load();
 
-	sf::RenderWindow window(sf::VideoMode(480/2, 700/2, 16), "NovelTea Launcher");
 	NovelTea::EngineConfig config;
 	config.width = 480;
 	config.height = 700;
 	config.fontSizeMultiplier = GSettings.getFontSizeMultiplier();
-	config.fps = 30;
-	config.initialState = NovelTea::StateID::Intro;
+	if (argc >= 5)
+		config.initialState = NovelTea::StateID::Main;
+	else
+		config.initialState = NovelTea::StateID::TitleScreen;
+	
 	auto engine = new NovelTea::Engine(config);
 	engine->initialize();
+	
+	// NovelTeaLaunch project.ntp entity typeId nameId [segmentId]
+	if (argc >= 5) {
+		auto command = std::string{argv[2]};
+		if (command == "entity") {
+			auto typeId = atoi(argv[3]);
+			auto idName = argv[4];
+			Proj.loadFromFile(argv[1]);
+			GSave->data()[NovelTea::ID::entrypointEntity] = sj::Array(typeId, idName);
+			if (argc == 6) {
+				auto segmentId = atoi(argv[5]);
+				GSave->data()[NovelTea::ID::entrypointMetadata] = sj::Array("", sj::Array(segmentId));
+			}
+		} else {
+			std::cout << "Unknown command: " << argv[2] << std::endl;
+			return 1;
+		}
+	} else if (argc == 2) {
+		std::string fileName = argv[1];
+		Proj.loadFromFile(fileName);
+	} else if (argc == 1) {
+	} else {
+		return 1;
+	}
+	
+	sf::RenderWindow window(sf::VideoMode(480/2, 700/2, 16), "NovelTea Launcher");
 	
 	GSave->setDirectory(dir);
 	while (window.isOpen())
