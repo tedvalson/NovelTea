38f84c84b7f489557fb14a123ce5aa4f9fb41409
Thomas Edvalson <machin3@gmail.com>
2022-08-01 17:44:45 -0400

    Tests: Add some more BBCode tests.

--- test/BBCodeParser.cpp
+++ test/BBCodeParser.cpp
@@ -4,17 +4,28 @@
 
 using namespace NovelTea;
 
-bool testConversion(const std::string &bbstring, const std::string &expected)
+bool testConversion(const std::string &bbstring, const std::string &expected, bool closeTags = false)
 {
 	auto segments = BBCodeParser::makeSegments(bbstring);
-	return (expected == BBCodeParser::makeString(segments));
+	auto str = BBCodeParser::makeString(segments, true, closeTags);
+	return (expected == str);
 }
 
-void testConversion(const std::string &bbstring)
+bool testConversion(const std::string &bbstring)
 {
-	testConversion(bbstring, bbstring);
+	return testConversion(bbstring, bbstring);
 }
 
 TEST(BBCodeParser, Conversion) {
+	EXPECT_TRUE(testConversion("[i]te[/i]st"));
+	EXPECT_TRUE(testConversion("te[/i]st", "test"));
 	EXPECT_TRUE(testConversion("[i]te[i]st", "[i]test"));
+	EXPECT_TRUE(testConversion("[i]test", "[i]test[/i]", true));
+	EXPECT_TRUE(testConversion("[i]te[i]st[/i]1", "[i]test1"));
+	EXPECT_TRUE(testConversion("[i]te[p]st"));
+	EXPECT_TRUE(testConversion("[i]te[p=1]st"));
+}
+
+TEST(BBCodeParser, AnimConversion) {
+	EXPECT_TRUE(testConversion("[a1 e=p cs=0 t=1]test", "[a1 cs=0 e=p t=1]test[/a1]", true));
 }
